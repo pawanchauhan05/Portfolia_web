306c55c78c04a2203b49565253699979e20c1719
pawan singh chauhan <pawansinghchouhan052gmail.com>
2017-01-08 13:22:31 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pawan Singh Chauhan</title>
+	<script src="js/jquery.min.js"> </script>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- start-smoth-scrolling -->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+		</script>
+        <script language="javascript">
+		function goPrint()
+		{
+			print();
+		}	
+		</script>
+ 	<!-- start-smoth-scrolling-->
+</head>
+<body>
+<!--headerbg starts-->
+ <div id="top-top" class="headerbg">
+	 <!--header starts-->
+	   <div class="header">
+		 <!--container starts-->
+			  <div class="container">
+				   <div class="logo">
+						<a href="#" onclick="goPrint()">Resume</a>
+				   </div>
+				  <!--Navigation starts-->
+				  <div class="nav">
+					 <ul>
+						 <li><a class="scroll" href="#about">ABOUT</a></li>
+                         <li><a class="scroll" href="#strength">STRENGTH</a></li>
+						 <li><a class="scroll" href="#skills">SKILLS</a></li>
+						 <li><a class="scroll" href="#port">PORTFOLIO</a></li>
+						 <li><a class="scroll" href="#contact">CONTACT</a></li>
+					 </ul>
+				 </div>
+				  <!--Navigation ends-->
+					<div class="clear"></div>
+			  </div>
+		 <!--container ends-->
+	   </div>
+	 <!--header ends-->
+	 <!--Banner starts-->
+	   <div class="banner">
+	      <div class="container">
+		     <div class="banner-info">
+			      <div class="bannerlogo">
+					 <img src="images/pawan.JPG" class="img-circle" alt="Circular Image" width="47" height="48"> 
+			      </div>
+			      <div class="bannerhead">
+                
+				      <h2>My Name is <span>Pawan Singh</span></h2>
+				      <h3>Full Stack Developer</h3>
+				      <p>An adaptable and knowledgeable person seeking a career which will improve skills and knowledge. Highly motivated, flexible, smart and hungry to learn new technologies, strategies, processes and methodologies, have a genuine interest in software developing</p>
+					  <a class="downarrow scroll hidden-xs" href="#about"><span> </span></a>
+			      </div>
+			 </div>
+			 <div class="clear"></div>
+		  </div>
+	  </div>
+	  <!--Banner ends-->
+ </div>
+ <!--Headerbg ends-->
+ <!--Content starts-->
+ <div class="content">
+	 <!--About starts-->
+	   <div id="about" class="about">
+		 <div class="container">
+			   <div class="header-section text-center">
+				  <h2><span> </span>ABOUT<span> </span></h2>
+				  </div>
+				 <span><p>An open-minded, level headed person who knows how to prioritize first things first. I am good at disposition, good at adapting, eager to learn. I enjoy overcoming challenges and also enjoy working on own initiative or in a team.</p></span>
+
+				<div class="years">	 
+					<h4><b>15 Mar 2016 - yet</b></h4>
+					 <span><h4>Android Developer</h4></span>
+					 <p><i>Currently Working As Android Developer At Enviguor Healthcare technologies Pvt. LTD</i></p>
+	            </div>
+
+	            <div class="years">	 
+					<h4><b>Sept 2015 - Feb 2016</b></h4>
+					 <span><h4>PG Diploma</h4></span>
+					 <p><i>Successfully completed with achieving 68 % marks From &nbsp;&nbsp;
+                 Board of Secondary Education, Rajasthan</i></p>
+	            </div>
+				
+              <div class="years">	 
+				<h4><b>March 2015 - Aug 2015</b></h4>
+				 <span><h4>Web Developer & Designer</h4></span>
+				 <p><i>Worked As Web Developer & Designer At Kit Infosoft Pvt. LTD</i></p>
+              </div>
+                	 
+			  <div class="years">	 
+				<h4><b>2010-2014</b></h4>
+				 <span><h4>B.Tech in Information Technology</h4></span>
+				 <p><i>Recently completed with achieving 68 % marks From Government Engineering college , Jhalawar </i></p>
+              </div>  
+              
+              <div class="years">
+              <h4><b>2013</b></h4>
+				 <span><h4>Summer Training</h4></span>
+				 <p><i>Successfully completed in J2EE at MICROSOFT TECNOLOGY ASSOSIATE, Noida </i></p>
+			  </div>
+				
+			  <div class="years">	 		
+				 <h4><b>2009-2010</b></h4>
+				 <span><h4>12th Standard with Science Maths</h4></span>
+				 <p><i>Successfully completed with achieving 78 % marks From &nbsp;&nbsp;
+                 Board of Secondary Education, Rajasthan </i></p>
+			  </div> 
+					
+			   <div class="years">	 	
+				 <h4><b>2007-2008</b></h4>
+				 <span><h4>10th Standard with All Subjects</h4></span>
+				 <div class="labs">
+					 <p><i>Successfully completed with achieving 75 % marks From <br>
+                     Board of Secondary Education, Rajasthan </i></p>
+			     </div>
+			  </div>
+				 <a class="arrow scroll" href="#strength"><span> </span></a>
+		  </div>
+	  </div>
+	 <!--About Ends-->
+     
+     <!-- Strenth Start -->
+     <div class="container" id="strength">
+     <div class="skill-section text-center">
+		<h2><span> </span>STRENGTHS<span> </span></h2>
+	 </div>
+     <br>
+     <p>
+     <center>
+     <i><h4>Good oral, written and presentation skills</h4></i>
+     <i><h4>Lead and work as a team in an organized way</h4></i>
+     <i><h4>Quick learner, action-oriented and result-focused</h4></i>
+     <i><h4>A high tolerance of stress and enjoy responsibilities</h4></i>
+     <i><h4>Great time management skills</h4></i>
+     </center>
+     </p>
+     <div class="clear"> </div>
+     <center> <a class="portdown scroll" href="#skills"><span> </span></a></center>
+     </div>
+     
+     <!-- Strenth End -->
+	 
+	<!--SKILLS Starts-->
+	  <div id="skills" class="skills">
+	  <div class="container">
+	     <div class="skills-grids">
+			  <div class="skill-section text-center">
+				  <h2><span> </span>SKILLS<span> </span></h2>
+			  </div>
+					  <div class="services_grids">
+						  <div id="canvas">
+								<div class="skill-grids text-center">
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-1"></div>									
+												 <h3>Android</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-2"></div>									
+												 <h3>Java</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-3"></div>									
+												 <h3>IOS</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>PHP</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>HTML 5</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>CSS</h3>	
+											 </div>								  
+										</div>
+										<div class="clearfix"> </div>
+								</div>
+
+								<div class="skill-grids text-center">
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-1"></div>									
+												 <h3>JS</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-2"></div>									
+												 <h3>Jquery</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-3"></div>									
+												 <h3>Angular</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>Bootstrap</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>C</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>C++</h3>	
+											 </div>								  
+										</div>
+										<div class="clearfix"> </div>
+								</div>
+
+								<div class="skill-grids text-center">
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-1"></div>									
+												 <h3>SQL</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-2"></div>									
+												 <h3>J2EE</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-3"></div>									
+												 <h3>Laravel</h3>	
+											 </div>								  
+										</div>
+										<div class="col-md-2">	
+											<div class="skill-grid">
+												<div class="circle" id="circles-4"></div>									
+												 <h3>Codeigniter</h3>	
+											 </div>								  
+										</div>
+										<div class="clearfix"> </div>
+								</div>
+
+						   </div>
+					  </div>
+					
+					 <!-- <script type="text/javascript" src="web/js/circles.js"></script>
+								 <script>
+									var colors = [
+											['#181115', '#EFEFEF'], ['#181115', '#EFEFEF'], ['#181115', '#EFEFEF'], ['#181115', '#EFEFEF']
+										];
+									for (var i = 1; i <= 5; i++) {
+										var child = document.getElementById('circles-' + i),
+											percentage = 50 + (i * 10);
+											
+										Circles.create({
+											id:         child.id,
+											percentage: percentage,
+											radius:     80,
+											width:      15,
+											number:   	percentage / 1,
+											text:       '%',
+											colors:     colors[i - 1]
+										});
+									}
+							
+					</script> -->
+			 </div>
+		 </div>
+		   <a class="down scroll" href="#port"><span> </span></a>
+	  </div>
+	  </div>
+	 <!--SKILLS Ends-->
+     
+
+     
+     
+     
+		 <!--PORTFOLIO Starts-->
+		 <div id="port" class="Portfolio">
+			  <div class="Portfolio-section text-center">
+			      <h2><span> </span>PORTFOLIO<span> </span></h2>
+			  </div>
+			  <div class="container">
+				  <div class="portfolio-grids">
+					  <div class="portfolio-grid col-md-4">
+						 <a href="#"><img src="images/monitr1.png"></a>
+						  <p>FitterFox Android App<br>
+                          Android, Java, XML</p>
+						  <a class="rightarrow" href="http://www.bardanatrade.com/" target="_blank"><span> </span></a>
+					  </div>
+					  <div class="portfolio-grid col-md-4">
+						  <a href="#"><img src="images/monitr2.png"></a>
+						  <p>School Diary Android App<br>
+                          Android, Java, XML</p>
+						  <a class="rightarrow" href="www.enquirytm.com" target="_blank"><span> </span></a>
+					  </div>
+					  <div class="portfolio-grid col-md-4">
+						  <a href="#"><img src="images/monitr3.png"></a>
+						  <p>EnquiryTM (Responsive)<br>
+						  Laravel, MySQL, Bootstrap, PHP</p>
+						  <a class="rightarrow" href="#"><span> </span></a>
+					  </div>
+					  <div class="clear"> </div>
+				  </div>
+				  <div class="portfolio-grids">
+					  <div class="portfolio-grid col-md-4">
+						 <a href="#"><img src="images/monitr1.png"></a>
+						  <p>Bardana Trade (Responsive)<br>
+                          Codeigniter, MySQL, Bootstrap, PHP</p>
+						  <a class="rightarrow" href="http://www.bardanatrade.com/" target="_blank"><span> </span></a>
+					  </div>
+					  <div class="portfolio-grid col-md-4">
+						  <a href="#"><img src="images/monitr2.png"></a>
+						  <p>Airlines Reservation System<br>
+                          JSP, Servlet, Java, MySQL</p>
+						  <a class="rightarrow" href="www.enquirytm.com" target="_blank"><span> </span></a>
+					  </div>
+					  <div class="portfolio-grid col-md-4">
+						  <a href="#"><img src="images/monitr3.png"></a>
+						  <p>Online Banking System<br>
+                          JSP, Servlet, Java, MySQL</p>
+						  <a class="rightarrow" href="#"><span> </span></a>
+					  </div>
+					  <div class="clear"> </div>
+				  </div>
+				  <a class="portdown scroll" href="#contact"><span> </span></a>
+				  
+			  </div>
+		 </div>
+  </div>
+  <!--Content Ends-->
+  
+  <!--FOOTER Starts-->
+  <div  id="contact" class="footer">
+      <div class="container">
+	  
+		   <div class="contact-section text-center">
+			  <h2><span> </span>CONTACT<span> </span></h2>
+		  </div>
+		 <div class="row">
+		 
+			 <div class="col-md-6">
+				  <div class="footer-left">
+						<div class="social-icons">
+						   <a href="#"><img src="images/in.png"></a>
+						   <a href="#"><img src="images/twt.png"></a>
+						   <a href="#"><img src="images/fb.png"></a>
+					   </div>
+                       
+					   <p>
+                       <font style="font-size:22px">Address and Phone</font><br>
+                       Behind Chambal Colony,House No. 365<br>
+                       Sakatpura , Kota (Rajasthan) <br>
+                       Pincode : 324008 <br>
+                       Email : Pawansinghchouhan05@gmail.com <br>
+                       Mobile : +91-9772217799
+                       </p>
+                       
+				  </div>
+			 </div>
+			  
+			  <div class="col-md-6">
+				  <div class="footer-right">
+				 <div class="form">
+				 <form action="" method="post">
+					 <input type="text" class="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+					 <input type="text" class="text" value="E-mail" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'E-mail';}">
+					 <textarea rows="2" cols="70" onfocus="if(this.value == 'Your Message') this.value='';" onblur="if(this.value == '') this.value='Your Message';" >Your Message</textarea>
+					 <div class="clear"> </div>
+					 <input type="submit" value="SEND"> 
+				 </form>
+				 </div>
+				  </div>
+				  <div class="clear"></div>
+				  
+			  </div>
+		 </div>
+	 </div>
+	 <a class="up scroll" href="#top-top"><span> </span></a>
+	  
+ </div>
+ 
+ </body>
+ </html>
+ 
+   
